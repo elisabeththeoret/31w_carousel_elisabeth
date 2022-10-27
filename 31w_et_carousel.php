0785593c5b2c6ccb92492ecbbf941dc5d79dde69
elisabeththeoret <101665871+elisabeththeoret@users.noreply.github.com>
2022-10-27 14:30:29 -0400

    c9 - Renommer le plugin
    Renommer le plugin et modifier tous les endroits où le nom était écrit.
    
    c9

--- 31w_et_carousel.php
+++ 31w_et_carousel.php
@@ -1,19 +1,31 @@
 <?php
 /**
  * 
- * @package 31w Carousel Elisabeth
+ * @package 31w ET Carousel
  * @author Elisabeth Theoret
  * @copyright Collège Maisonneuve
  * @license GLP-2.0-or-later
  * @version 1.0.0
 */
 /*
-Plugin Name: 31w Carousel Elisabeth
+Plugin Name: 31w ET Carousel
 Description: Une extension simple qui permet d'afficher une boîte modale de l'image sélectionnée d'une galerie, pour ensuite accéder à toutes les images de la galerie.
 Author: Elisabeth Theoret
-Author URI: https://github.com/elisabeththeoret/31w_carousel_elisabeth
+Author URI: https://github.com/elisabeththeoret/31w_et_carousel
 Version: 1.0.0
 */
+
+
+// filemtime(); // retourne en milliseconde le temps de la dernière sauvegarde
+// plugin_dir_path(); // retourne le chemin du répertoire du plugin
+// __FILE__; // le fichier en train de s'exécuter
+// wp_enqueue_style(); // Intègre le link:css dans la page
+// wp_enqueue_script(); // intègre le script dans la page
+// wp_enqueue_scripts(); // le hook
+
+function et_enqueue(){}
+$version_css = '';
+$version_js = '';
 
 function boite_carousel(){
 	/////////////////////////////////////// HTML
@@ -27,4 +39,4 @@ function boite_carousel(){
 
 	return $contenu;
 }
-add_shortcode( '31w_carousel_elisabeth', 'boite_carousel' );
+add_shortcode( '31w_et_carousel', 'boite_carousel' );
