6008f6c6be8b30b581278e857f17aebd1f3bf3ea
elisabeththeoret <101665871+elisabeththeoret@users.noreply.github.com>
2022-10-27 17:21:49 -0400

    c9 - Ajouter des commentaires dans le fichier carrousel.js, documentation
    Ajouter des commentaires dans le fichier carrousel.js pour la documentation.
    
    c9

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,3 +1,6 @@
+/**
+ * Fonction qui initialise le carrousel
+ */
 function Initialisation() {
 	console.log( "carrousel" );
 
@@ -5,12 +8,18 @@ function Initialisation() {
 	const boutonOuvrir = document.querySelector( ".carrousel__bouton--ouvrir" );
 	const boutonFermer = document.querySelector( ".carrousel__bouton--fermer" );
 
+	/**
+	 * Écouteur d'événement au click du bouton ouvrir
+	 */
 	boutonOuvrir.addEventListener( "click", function() {
 		console.log( "ouvrir carrousel" );
 		carrousel.classList.remove( "carrousel--fermer" );
 		carrousel.classList.add( "carrousel--ouvrir" );
 	} );
-	
+
+	/**
+	 * Écouteur d'événement au click du bouton fermer
+	 */
 	boutonFermer.addEventListener( "click", function() {
 		console.log( "fermer carrousel" );
 		carrousel.classList.remove( "carrousel--ouvrir" );
@@ -18,4 +27,7 @@ function Initialisation() {
 	} );
 }
 
+/**
+ * Écouteur d'événement au chargement de la fenêtre
+ */
 window.addEventListener( "load", Initialisation );
