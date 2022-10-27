735083200223e50b4a4f43367346a72c286baa5b
elisabeththeoret <101665871+elisabeththeoret@users.noreply.github.com>
2022-10-27 16:18:06 -0400

    c9 - Ajouter l'interaction des boutons du carrousel
    Ajout des eventListener sur les boutons pour créer l'interactivité du carrousel.
    
    c9

--- js/carrousel.js
+++ js/carrousel.js
@@ -2,8 +2,19 @@ function Initialisation() {
 	console.log( 'carrousel' );
 
 	const boutonOuvrir = document.querySelector( ".carrousel__bouton--ouvrir" );
+	const boutonFermer = document.querySelector(".carrousel__bouton--fermer")
+	const carrousel = document.querySelector(".carrousel");
+
 	boutonOuvrir.addEventListener( 'click', function() {
 		console.log( 'ouvrir carrousel' );
+		carrousel.classList.remove( "carrousel--fermer" );
+		carrousel.classList.add( "carrousel--ouvrir" );
+	} );
+
+	boutonFermer.addEventListener( "click", function() {
+		console.log( 'fermer carrousel' );
+		carrousel.classList.remove( "carrousel--ouvrir" );
+		carrousel.classList.add( "carrousel--fermer" );
 	} );
 }
 
