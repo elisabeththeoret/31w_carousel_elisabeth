5f769e96680d31c32f71b9d00ede464ae98f4f3e
elisabeththeoret <101665871+elisabeththeoret@users.noreply.github.com>
2022-11-10 12:39:51 -0500

    c11 - Remanier les fonctions de bases et créer de nouvelles fonctions afin d'améliorer le code
    Fonction qui affiche le carrousel.
    Fonction qui ajoute les images (une à la fois maintenant).
    Fonction qui ajoute les boutons radios pour chaque image (indexImage).
    
    c11

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,30 +1,76 @@
 /**
  * Fonction qui initialise le carrousel
  */
-function Initialisation() {
+ ( function() {
 	console.log( "carrousel" );
 
 	const carrousel = document.querySelector( ".carrousel" );
 	const cFigure = document.querySelector( ".carrousel__figure" );
+	const cForm = document.querySelector( '.carrousel__form' );
 	const cBoutonOuvrir = document.querySelector( ".carrousel__bouton--ouvrir" );
 	const cBoutonFermer = document.querySelector( ".carrousel__bouton--fermer" );
 
 	const galerie = document.querySelector( ".galerie" );
 	const galerieImages = document.querySelectorAll( ".galerie img" );
+	console.log(galerieImages);
 
-	let index = 0;
+	let indexImage = 0;
+
 
 	/**
 	 * Ouvrir le carrousel
 	 */
 	cBoutonOuvrir.addEventListener( "click", function() {
-		console.log( "ouvrir carrousel" );
+		afficherCarrousel();
+	} );
 
-		ajouterImages();
+	/**
+	 * Créer et afficher le carrousel
+	 */
+	function afficherCarrousel() {
+		console.log( "ouvrir carrousel" );
 
 		carrousel.classList.remove( "carrousel--fermer" );
 		carrousel.classList.add( "carrousel--ouvrir" );
-	} );
+
+		for ( const uneImage of galerieImages ) {
+			ajouterImages( uneImage );
+			ajouterRadios();
+			indexImage++;
+		}
+
+	}
+
+	/**
+	 * Fonction qui ajoute les images de la galerie dans le carrousel
+	 * @param { HTML Element } uneImage
+	 */
+	function ajouterImages( uneImage ) {
+
+		uneImage.dataset.index = indexImage;
+		let cImage = document.createElement( "img" );
+		cImage.setAttribute( "src", uneImage.getAttribute( "src" ) );
+		cImage.classList.add( "carrousel__figure__img" );
+
+		cFigure.appendChild( cImage );
+
+	}
+
+	/**
+	 * Fonction qui crée les boutons radios pour chaque image de la galerie du carrousel et les ajoute dans le formulaire de la navigation
+	 */
+	function ajouterRadios() {
+
+		let cRadio = `
+			<div class="carrousel__radio">
+				<input type="radio" class="carrousel__form__radio" name="carrousel__form__radio" data-index="${ indexImage }">
+				<span class="carrousel__radio__custom"></span>
+			</div>
+		`;
+
+		cForm.innerHTML += cRadio;
+
+	}
 
 	/**
 	 * Fermer le carrousel
@@ -34,31 +80,8 @@ function Initialisation() {
 
 		carrousel.classList.remove( "carrousel--ouvrir" );
 		carrousel.classList.add( "carrousel--fermer" );
+
 	} );
 
-	/**
-	 * Fonction qui ajoute les images de la galerie dans le carrousel
-	 */
-	function ajouterImages() {
-		let i = 0;
+} )();
 
-		for ( const uneImage of galerieImages ) {
-			i++;
-
-			let cImage = document.createElement( "img" );
-			cImage.setAttribute( "src", uneImage.getAttribute( "src" ) );
-			cImage.classList.add( "carrousel__figure__img" );
-			cImage.dataset.index = i;
-
-			cFigure.appendChild( cImage );
-		}
-
-		carrousel.appendChild( cFigure );
-	}
-
-}
-
-/**
- * Écouteur d'événement au chargement de la fenêtre
- */
-window.addEventListener( "load", Initialisation );
